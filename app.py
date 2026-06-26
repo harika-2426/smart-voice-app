1ee78cbeafead2ba9ec84775b008e4bab158ef17
Harika Munjeti <harikamunjeti915@gmail.com>
2026-06-26 22:29:56 +0530

    updated project changes

--- app.py
+++ app.py
@@ -1,86 +1,39 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request
 from deep_translator import GoogleTranslator
-import speech_recognition as sr
 from gtts import gTTS
-import os
-
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET","POST"])
 def home():
-
     translation = ""
     audio_file = ""
 
-
     if request.method == "POST":
-
         text = request.form.get("text")
-
         source = request.form.get("source")
-
         target = request.form.get("target")
 
-
         try:
-
             translation = GoogleTranslator(
                 source="auto" if source=="auto" else source,
                 target=target
             ).translate(text)
 
-
-            # create voice output
-
-            speech = gTTS(
-                text=translation,
-                lang=target
-            )
-
+            speech = gTTS(text=translation, lang=target)
             audio_file = "translation.mp3"
+            speech.save("static/" + audio_file)
 
-            speech.save(
-                "static/" + audio_file
-            )
-
-
-        except Exception as e:
-
+        except:
             translation = "Error occurred"
 
-
-
-    return render_template(
-        "index.html",
-        translation=translation,
-        audio=audio_file
-    )
-
-
+    return render_template("index.html", translation=translation, audio=audio_file)
 
 
 @app.route("/voice")
 def voice():
-
-    recognizer = sr.Recognizer()
-
-
-    with sr.Microphone() as source:
-
-        audio = recognizer.listen(source)
+    return "Voice input not supported on cloud."
 
 
-    try:
-
-        text = recognizer.recognize_google(audio)
-
-        return text
-
-
-    except:
-
-        return "Could not understand"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
