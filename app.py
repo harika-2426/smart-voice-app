78dc6d5b32cd587d03e790db0f621beb0db7371a
Harika Munjeti <harikamunjeti915@gmail.com>
2026-06-26 22:13:11 +0530

    updated project changes

--- app.py
+++ app.py
@@ -82,10 +82,5 @@ def voice():
     except:
 
         return "Could not understand"
-
-
-
-
-if __name__=="__main__":
-
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
